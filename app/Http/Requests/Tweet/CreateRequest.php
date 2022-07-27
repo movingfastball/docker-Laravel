11dc06c3a1facd8375fe7806c69573a57670a086
movingfastball <movingfastballny2012@yahoo.co.jp>
2022-07-27 18:44:07 +0900

    02

---
+++ app/Http/Requests/Tweet/CreateRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests\Tweet;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'tweet' => 'required|max:140'
+        ];
+    }
+
+    public function tweet(): string
+    {
+        return $this->input('tweet');
+    }
+
+}
