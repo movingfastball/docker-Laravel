8b5828c7ce1d6fdde97a4b6108946d409db9d951
movingfastball <movingfastballny2012@yahoo.co.jp>
2022-07-29 23:07:02 +0900

    いったんコミット

--- database/factories/TweetFactory.php
+++ database/factories/TweetFactory.php
@@ -2,28 +2,25 @@
 
 namespace Database\Factories;
 
-use App\Models\Tweet;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Carbon\Carbon;
 
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tweet>
+ */
 class TweetFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = Tweet::class;
-
-    /**
      * Define the model's default state.
      *
-     * @return array
+     * @return array<string, mixed>
      */
     public function definition()
     {
         return [
             'user_id' => 1, // つぶやきを投稿したユーザーのIDをデフォルトで1とする
-            'content' => $this->faker->realText(100)
+            'content' => $this->faker->realText(100),
+            'created_at' => Carbon::now()->yesterday()
         ];
     }
 }
